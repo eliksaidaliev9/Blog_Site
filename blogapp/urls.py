9a31cb5247bb9979783bcd922c687930891957a8
Elyor <elik.saidaliev9@gmail.com>
2025-09-20 00:48:38 +0500

    Initial commit: Created blog website

---
+++ blogapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+
+    path('', views.PostListView.as_view()),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
+         views.post_detail, name='post_detail'),
+]
